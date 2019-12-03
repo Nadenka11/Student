87edd6dd27f480379e95aed0488967458be23d16
Nadenka11 <57888500+Nadenka11@users.noreply.github.com>
2019-12-03 13:47:53 +0200

    Add files via upload

---
+++ build/precache-manifest.64a804dd22e803880a3916ae89bec91a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b7d5e08b8e12508489805f7199e804",
+    "url": "/index.html"
+  },
+  {
+    "revision": "840e252b5c294c068c25",
+    "url": "/static/js/2.2ddd2799.chunk.js"
+  },
+  {
+    "revision": "adeabfee6b98cdfd20e9",
+    "url": "/static/js/main.e4a08122.chunk.js"
+  },
+  {
+    "revision": "507bb9a5ed930ce05b2d",
+    "url": "/static/js/runtime-main.bd8ec6ac.js"
+  }
+]);
---
+++ src/screens/StudentsListScreen/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+import {
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    Avatar,
+    ListSubheader,
+    Fab,
+    makeStyles, Button,
+} from '@material-ui/core';
+import {AddBoxSharp as AddIcon, DeleteForever} from '@material-ui/icons';
+import StudentsList from '../common/StudentsList';
+import {Link} from 'react-router-dom';
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import IconButton from "@material-ui/core/IconButton";
+import {deleteStudent} from "../../services/APIService";
+
+const useStyles = makeStyles(theme => ({
+    list: {
+        paddingBottom: theme.spacing(10),
+    },
+    fab: {
+        position: 'fixed',
+        bottom: theme.spacing(2),
+        right: theme.spacing(2),
+    },
+}));
+
+export default () => {
+    const classes = useStyles();
+    return (
+        <>
+            <List className={classes.list}>
+                <ListSubheader>Students</ListSubheader>
+                <StudentsList>
+                    {({students}) =>
+                        students.map(student => (
+                            <ListItem
+                                key={student.id}
+                                button
+                                component={Link}
+                                to={`/students/${student.id}`}
+                            >
+                                <ListItemAvatar>
+                                    <Avatar src={student.avatar}/>
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary={student.name}
+                                    secondary={student.address}
+                                />
+                                <ListItemSecondaryAction>
+                                    <IconButton aria-label="Delete Todo"
+                                                component={Link}
+                                                to={`/students/delete/${student.id}`}
+                                                color="secondary"
+                                                variant="outlined">
+                                        <DeleteForever />
+                                    </IconButton>
+                                </ListItemSecondaryAction>
+                            </ListItem>
+                        ))
+                    }
+                </StudentsList>
+            </List>
+            <Fab
+                className={classes.fab}
+                color="primary"
+                component={Link}
+                to="/students/create"
+            >
+                <AddIcon/>
+            </Fab>
+        </>
+    );
+};
